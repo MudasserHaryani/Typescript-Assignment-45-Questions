c3301e24db2043f2fe74b57fde9f858a5fda64da
MudasserHaryani <160148229+MudasserHaryani@users.noreply.github.com>
2024-03-09 23:01:17 +0500

    Add files via upload

---
+++ Q2 Personal Message/E2-Personal_Message.js
@@ -0,0 +1,5 @@
+"use strict";
+//Exercise # 2
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let PersonName = "Mudasser Haryani";
+console.log(`Hello, ${PersonName}`);
