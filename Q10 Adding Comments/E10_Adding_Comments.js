01b1aebdc748e0fc3bafcba79777836680d11e8f
MudasserHaryani <160148229+MudasserHaryani@users.noreply.github.com>
2024-03-09 23:17:08 +0500

    Add files via upload

---
+++ Q10 Adding Comments/E10_Adding_Comments.js
@@ -0,0 +1,17 @@
+"use strict";
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// Program Name: Simple Addition
+// Author: Your Name
+// Date: February 12, 2024
+// Description: This program performs a simple addition operation.
+let num1 = 5;
+let num2 = 3;
+let sum = num1 + num2;
+console.log("Sum:", sum);
+// Program Name: Greeting Message
+// Author: Your Name
+// Date: February 12, 2024
+// Description: This program greets the user with a personalized message.
+let userName = "Mudasser Haryani";
+let greetingMessage = `Hello, ${userName}!`;
+console.log(greetingMessage);
---
+++ Q10 Adding Comments/E10_Adding_Comments.ts
@@ -0,0 +1,24 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+
+// Program Name: Simple Addition
+// Author: Your Name
+// Date: February 12, 2024
+// Description: This program performs a simple addition operation.
+
+let num1: number = 5;
+let num2: number = 3;
+let sum: number = num1 + num2; 
+
+console.log("Sum:", sum); 
+
+// Program Name: Greeting Message
+// Author: Your Name
+// Date: February 12, 2024
+// Description: This program greets the user with a personalized message.
+
+let userName: string = "Mudasser Haryani"; 
+let greetingMessage: string = `Hello, ${userName}!`; 
+
+console.log(greetingMessage); 
