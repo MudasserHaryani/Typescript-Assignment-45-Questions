79331773dee198dac426d4657533517733567c22
MudasserHaryani <160148229+MudasserHaryani@users.noreply.github.com>
2024-03-09 23:10:10 +0500

    Add files via upload

---
+++ Q5 Famous Quote2/E5_Famous_Quote2.js
@@ -0,0 +1,8 @@
+"use strict";
+//Exercise#5
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Albert Einstien";
+let famous_quote = "A person who never made a mistake never tried anything new.";
+let authorAge = 59;
+let message = `${famous_person} once said, ${famous_quote} at his age of "${authorAge}"`;
+console.log(message);
---
+++ Q5 Famous Quote2/E5_Famous_Quote2.ts
@@ -0,0 +1,13 @@
+//Exercise#5
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+
+let famous_person: string = "Albert Einstien";
+let famous_quote: string = "A person who never made a mistake never tried anything new.";
+let authorAge: number = 59;
+let message: string = `${famous_person} once said, ${famous_quote} at his age of "${authorAge}"`;
+console.log(message);
+
+
+
