88812f8053649226ee02a37243904b1a1f38d5f0
MudasserHaryani <160148229+MudasserHaryani@users.noreply.github.com>
2024-03-11 00:29:41 +0500

    Add files via upload

---
+++ Q11 Names/E11_Names.ts
@@ -0,0 +1,14 @@
+//Exercise#11
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+
+// Store the names of a few of your friends in a array called names
+let names: string[] = ["Ali", "Zain", "Saadat", "Kashif"];
+
+// Print each person’s name by accessing each element in the list, one at a time
+for (let i = 0; i < names.length; i++) {
+  // i is the index of the current element
+  // names[i] is the value of the current element
+  console.log(names[i]);
+}
+
