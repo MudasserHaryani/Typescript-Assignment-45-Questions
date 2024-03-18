2516048f5dd70046dc52be73548127ae2ab4528a
MudasserHaryani <160148229+MudasserHaryani@users.noreply.github.com>
2024-03-19 00:08:36 +0500

    Add files via upload

---
+++ Q38_cities/main.ts
@@ -0,0 +1,9 @@
+// Define a function called describe_city with a default parameter for the country
+function describe_city(city: string, country: string = "Pakistan"): void {
+    console.log(`${city} is in ${country}.`);
+}
+
+// Call the function for three different cities
+describe_city("Karachi");
+describe_city("Lahore");
+describe_city("New York", "USA");
