07c70477b1365774d57b904684ae6fcd6a1e4783
MudasserHaryani <160148229+MudasserHaryani@users.noreply.github.com>
2024-03-18 22:39:52 +0500

    Add files via upload

---
+++ Q22 _Intentional_Error/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+let myArray = [1, 2, 3, 4, 5];
+// Trying to access an index that is out of bounds
+let indexError = myArray[10]; // This will produce an array index error
+// Correcting the error
+console.log(indexError); // This line will not be executed because the error will stop the execution flow
