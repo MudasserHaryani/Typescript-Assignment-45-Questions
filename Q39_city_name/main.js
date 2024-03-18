3f86a674ad7184b4410de744a515c4308625bae1
MudasserHaryani <160148229+MudasserHaryani@users.noreply.github.com>
2024-03-19 00:10:22 +0500

    Add files via upload

---
+++ Q39_city_name/main.js
@@ -0,0 +1,8 @@
+// Define a function called city_country
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+// Define a function called city_country
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "France"));
---
+++ Q39_city_name/main.ts
@@ -0,0 +1,11 @@
+
+
+// Define a function called city_country
+function city_country(city: string, country: string): string{
+    return `${city}, ${country}`;
+}
+
+// Define a function called city_country
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "France"));
