ae4019b0e99a655571bfd5b779d9ba71b0de0e3b
MudasserHaryani <160148229+MudasserHaryani@users.noreply.github.com>
2024-03-19 00:03:24 +0500

    Add files via upload

---
+++ Q36_t-shirt/main.js
@@ -0,0 +1,6 @@
+// Define a function called make_shirt
+function make_shirt(size, message) {
+    console.log("The shirt size is ".concat(size, " and the message printed on it is: ").concat(message));
+}
+// Call the function
+make_shirt("Large", "I love Pakistan!");
---
+++ Q36_t-shirt/main.ts
@@ -0,0 +1,7 @@
+// Define a function called make_shirt
+function make_shirt(size: string, message: string): void {
+    console.log(`The shirt size is ${size} and the message printed on it is: ${message}`);
+}
+
+// Call the function
+make_shirt("Large", "I love Pakistan!");
